b568edf636a051cc1726aee84a289ddc42cb9ee5
Riddler <luya.wang@qq.com>
2024-07-23 09:59:26 +0800

    add jinaai-reranker-v2-base-multiligual

--- src/models/reranking.rs
+++ src/models/reranking.rs
@@ -4,6 +4,8 @@ pub enum RerankerModel {
     BGERerankerBase,
     // jinaai/jina-reranker-v1-turbo-en
     JINARerankerV1TurboEn,
+    // jinaai/jina-reranker-v2-base-multilingual
+    JINARerankerV2BaseMultiligual,
 }
 
 pub fn reranker_model_list() -> Vec<RerankerModelInfo> {
@@ -18,6 +20,12 @@ pub fn reranker_model_list() -> Vec<RerankerModelInfo> {
             model: RerankerModel::JINARerankerV1TurboEn,
             description: String::from("reranker model for English"),
             model_code: String::from("jinaai/jina-reranker-v1-turbo-en"),
+            model_file: String::from("onnx/model.onnx"),
+        },
+        RerankerModelInfo {
+            model: RerankerModel::JINARerankerV2BaseMultiligual,
+            description: String::from("reranker model for multilingual"),
+            model_code: String::from("jinaai/jina-reranker-v2-base-multilingual"),
             model_file: String::from("onnx/model.onnx"),
         },
     ];
