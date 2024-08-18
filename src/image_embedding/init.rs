4ea0ae8ef1d824bce2218cdb1c548e5c7352dbc8
Anush <anushshetty90@gmail.com>
2024-08-19 00:22:12 +0530

    feat!: #[non_exhaustive] and builder (#112)
    BREAKING CHANGE: Struct expression is not allowed anymore.

--- src/image_embedding/init.rs
+++ src/image_embedding/init.rs
@@ -8,11 +8,39 @@ use super::{utils::Compose, DEFAULT_EMBEDDING_MODEL};
 
 /// Options for initializing the ImageEmbedding model
 #[derive(Debug, Clone)]
+#[non_exhaustive]
 pub struct ImageInitOptions {
     pub model_name: ImageEmbeddingModel,
     pub execution_providers: Vec<ExecutionProviderDispatch>,
     pub cache_dir: PathBuf,
     pub show_download_progress: bool,
+}
+
+impl ImageInitOptions {
+    pub fn new(model_name: ImageEmbeddingModel) -> Self {
+        Self {
+            model_name,
+            ..Default::default()
+        }
+    }
+
+    pub fn with_cache_dir(mut self, cache_dir: PathBuf) -> Self {
+        self.cache_dir = cache_dir;
+        self
+    }
+
+    pub fn with_execution_providers(
+        mut self,
+        execution_providers: Vec<ExecutionProviderDispatch>,
+    ) -> Self {
+        self.execution_providers = execution_providers;
+        self
+    }
+
+    pub fn with_show_download_progress(mut self, show_download_progress: bool) -> Self {
+        self.show_download_progress = show_download_progress;
+        self
+    }
 }
 
 impl Default for ImageInitOptions {
@@ -30,8 +58,23 @@ impl Default for ImageInitOptions {
 ///
 /// Model files are held by the UserDefinedImageEmbeddingModel struct
 #[derive(Debug, Clone, Default)]
+#[non_exhaustive]
 pub struct ImageInitOptionsUserDefined {
     pub execution_providers: Vec<ExecutionProviderDispatch>,
+}
+
+impl ImageInitOptionsUserDefined {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_execution_providers(
+        mut self,
+        execution_providers: Vec<ExecutionProviderDispatch>,
+    ) -> Self {
+        self.execution_providers = execution_providers;
+        self
+    }
 }
 
 /// Convert ImageInitOptions to ImageInitOptionsUserDefined
@@ -49,9 +92,19 @@ impl From<ImageInitOptions> for ImageInitOptionsUserDefined {
 ///
 /// The onnx_file and preprocessor_files are expecting the files' bytes
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 pub struct UserDefinedImageEmbeddingModel {
     pub onnx_file: Vec<u8>,
     pub preprocessor_file: Vec<u8>,
+}
+
+impl UserDefinedImageEmbeddingModel {
+    pub fn new(onnx_file: Vec<u8>, preprocessor_file: Vec<u8>) -> Self {
+        Self {
+            onnx_file,
+            preprocessor_file,
+        }
+    }
 }
 
 /// Rust representation of the ImageEmbedding model
--- src/sparse_text_embedding/init.rs
+++ src/sparse_text_embedding/init.rs
@@ -9,12 +9,45 @@ use super::{DEFAULT_EMBEDDING_MODEL, DEFAULT_MAX_LENGTH};
 
 /// Options for initializing the SparseTextEmbedding model
 #[derive(Debug, Clone)]
+#[non_exhaustive]
 pub struct SparseInitOptions {
     pub model_name: SparseModel,
     pub execution_providers: Vec<ExecutionProviderDispatch>,
     pub max_length: usize,
     pub cache_dir: PathBuf,
     pub show_download_progress: bool,
+}
+
+impl SparseInitOptions {
+    pub fn new(model_name: SparseModel) -> Self {
+        Self {
+            model_name,
+            ..Default::default()
+        }
+    }
+
+    pub fn with_max_length(mut self, max_length: usize) -> Self {
+        self.max_length = max_length;
+        self
+    }
+
+    pub fn with_cache_dir(mut self, cache_dir: PathBuf) -> Self {
+        self.cache_dir = cache_dir;
+        self
+    }
+
+    pub fn with_execution_providers(
+        mut self,
+        execution_providers: Vec<ExecutionProviderDispatch>,
+    ) -> Self {
+        self.execution_providers = execution_providers;
+        self
+    }
+
+    pub fn with_show_download_progress(mut self, show_download_progress: bool) -> Self {
+        self.show_download_progress = show_download_progress;
+        self
+    }
 }
 
 impl Default for SparseInitOptions {
@@ -33,9 +66,19 @@ impl Default for SparseInitOptions {
 ///
 /// The onnx_file and tokenizer_files are expecting the files' bytes
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 pub struct UserDefinedSparseModel {
     pub onnx_file: Vec<u8>,
     pub tokenizer_files: TokenizerFiles,
+}
+
+impl UserDefinedSparseModel {
+    pub fn new(onnx_file: Vec<u8>, tokenizer_files: TokenizerFiles) -> Self {
+        Self {
+            onnx_file,
+            tokenizer_files,
+        }
+    }
 }
 
 /// Rust representation of the SparseTextEmbedding model
